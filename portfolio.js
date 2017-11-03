39c20acbf0ec9c05d44a274dbb2382415e909ae2
McArthur Diaz <mc.arthur_d@hotmail.com>
2017-11-02 22:51:56 -0700

    added project

--- portfolio.js
+++ portfolio.js
@@ -3,3 +3,12 @@ $(document).ready(function() {
     $(".background").fadeIn(2000); 
 
 });
+
+
+$(document).ready(function() {
+    $(".title").animate({ 
+        left:'0px',
+        height:'0px',
+        width:'0px'
+    });
+});
